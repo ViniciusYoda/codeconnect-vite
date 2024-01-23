70b5251765b188d83682e6d4bdb4ac31fe3a64cd
Vinícius Yoda <vyoda4604@gmail.com>
2024-01-23 20:16:37 -0300

    fim

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import BarraDePesquisa from './componentes/BarraDePesquisa'
+import Card from './componentes/Card'
+import Filtro from './componentes/Filtro'
+import Ordenacao from './componentes/Ordenacao'
+import Sidebar from './componentes/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes')
+      .then(resposta => resposta.json())
+      .then(dados => setDados(dados))
+  }, [])
 
   return (
-    <>
+    <div className='container'>
+      <Sidebar />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BarraDePesquisa />
+        <Filtro />
+        <Ordenacao />
+        <ul className='lista-cards'>
+          {dados ? dados.map((item, index) => (
+            <li key={index}>
+              <Card
+                id={item.id}
+                imagemUrl={item.imagem_capa}
+                titulo={item.titulo}
+                resumo={item.resumo}
+                linhasDeCodigo={item.linhas_de_codigo}
+                compartilhamentos={item.compartilhamentos}
+                comentarios={item.comentarios}
+                usuario={item.usuario}
+              />
+            </li>
+          )) : null}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
